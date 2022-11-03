28327faad26fe42e42561386e24a240b4f17d131
larryerm7 <110516060+larryerm7@users.noreply.github.com>
2022-11-03 01:36:34 -0400

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+const array = ["Larry Reyes", 
+               26, 
+               true, 
+               new Date("23 March 1996"), 
+               myBook = {tituo: "Fundamentos del Diseño",
+                         autor: "Wucius Wong", 
+                         fecha: "1993",
+                         url: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjX9dzqoJH7AhXQSjABHcGOBgQQFnoECBgQAQ&url=http%3A%2F%2F132.248.48.64%2Frepositorio%2Fmoodle%2Fpluginfile.php%2F1546%2Fmod_resource%2Fcontent%2F6%2Fmateriales%2Fla_forma.pdf&usg=AOvVaw2QsoFJTHeEI71_gekHWWYv"
+                         }
+              ];
