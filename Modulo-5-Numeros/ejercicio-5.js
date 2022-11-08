8cd407f1a63b5c2b560af177fe4ecfd7e924df6b
larryerm7 <110516060+larryerm7@users.noreply.github.com>
2022-11-07 22:45:48 -0400

    Create ejercicio-5.js

---
+++ Modulo-5-Numeros/ejercicio-5.js
@@ -0,0 +1,18 @@
+alturaCm = 176;
+console.log(alturaCm);
+
+alturaMetro = 1.76;
+console.log(alturaMetro);
+
+peso = 76.2;
+console.log(peso);
+
+alturaMetroRedondeadoArriba = Math.ceil(alturaMetro);
+console.log(alturaMetroRedondeadoArriba);
+
+pesoRedondeadoAbajo = Math.floor(peso);
+console.log(pesoRedondeadoAbajo);
+
+maxValor_a = Number.MAX_VALUE + 1;
+maxValor_b = maxValor_a + 1;
+console.log(maxValor_a == maxValor_b);
