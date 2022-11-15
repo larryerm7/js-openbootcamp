c81fb4a15360c130dac1e87596161453584038a7
larryerm7 <110516060+larryerm7@users.noreply.github.com>
2022-11-15 09:07:47 -0400

    Create conjunto.js

---
+++ set_conjunto/conjunto.js
@@ -0,0 +1,7 @@
+// Nuevo Set con los nombres de mi familia.
+
+const familia = ["lennys" , "larry_jose", "yuliet","larry" ];
+const set_familia = new Set(familia);
+set_familia.add("larry");
+set_familia.add("Javascript");
+console.log(set_familia);
